a5f97c308aa03c489559f2d1b5fac073c3abbcc2
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-06-15 19:53:20 +0700

    singletion pattern

---
+++ creational/singletion-pattern.js
@@ -0,0 +1,47 @@
+const mySingleton = (function () {
+
+  // Instance stores a reference to the Singleton
+  let instance;
+
+  function init() {
+
+    // Singleton
+
+    // Private methods and variables
+    function privateMethod(){
+        console.log( "I am private" );
+    }
+
+    const privateRandomNumber = Math.random();
+
+    return {
+      // Public methods and variables
+      publicMethod: function () {
+        console.log( "The public can see me!" );
+      },
+      publicProperty: "I am also public",
+      getRandomNumber: function() {
+        return privateRandomNumber;
+      }
+
+    };
+
+  };
+
+  return {
+
+    // Get the Singleton instance if one exists or create one if it doesn't
+    getInstance: function () {
+
+      if ( !instance ) {
+        instance = init();
+      }
+
+      return instance;
+    }
+
+  };
+
+})();
+
+module.exports = mySingleton;
--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const CarBuilder = require('./builder-pattern');
+
+
+// Singleton pattern run.
+const mySingleton = require('./creational/singletion-pattern');
+
+var singleA = mySingleton.getInstance();
+var singleB = mySingleton.getInstance();
+
+console.log(singleA.getRandomNumber() === singleB.getRandomNumber()); // true
+console.log(singleA.publicProperty);
+
+
+/*
+// Builder pattern run.
+const CarBuilder = require('./creational/builder-pattern');
 
 const car = new CarBuilder()
     .addEngine('v12')
@@ -8,6 +22,7 @@ const car = new CarBuilder()
 
 const car1 = new CarBuilder().addEngine('v10').addBody('KIA SOLUTO LUXURY').build();
 
+console.log('Car', car);
+console.log('Car1', car1);
 
-console.log('Car', car)
-console.log('Car1', car1)
+*/
